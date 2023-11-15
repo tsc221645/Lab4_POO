746e42ccb6c7ca23d831fe31b28e4d45e85c27d7
Flaming Penguin <tsc221645@uvg.edu.gt>
2023-11-14 22:10:26 -0600

    lol

--- system/Main.java
+++ system/Main.java
@@ -1,7 +1,10 @@
 package system;
 
+import view.DriverProgram;
+
 public class Main {
     public static void main(String[] args) {
-        
+        DriverProgram ui = new DriverProgram();
+        ui.run();
     }
 }
